574597f7dc32d327cd33ab379a69acab14ea3e0c
olzlc <2305501037@qq.com>
2024-11-01 23:02:15 +0800

    前端跨域请求实现

---
+++ kaizhi-education/kaizhi-system/kaizhi-system-service/src/main/java/com/edu/kaizhi/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.edu.kaizhi.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.kaizhi.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
