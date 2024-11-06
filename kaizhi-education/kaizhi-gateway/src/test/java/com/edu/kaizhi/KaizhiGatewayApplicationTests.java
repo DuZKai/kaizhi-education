b44c41b58a91868a57c95d9499a4a8a424817316
olzlc <2305501037@qq.com>
2024-11-06 15:41:06 +0800

    网关配置加入，nacos配置更新

---
+++ kaizhi-education/kaizhi-gateway/src/test/java/com/edu/kaizhi/KaizhiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.kaizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizhiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
