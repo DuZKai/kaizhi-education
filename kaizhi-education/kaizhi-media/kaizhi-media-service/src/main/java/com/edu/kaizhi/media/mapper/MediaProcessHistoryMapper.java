fdb97dc7945bbf7f7f04d2787398b4fe75c5def8
olzlc <2305501037@qq.com>
2024-11-06 16:50:21 +0800

    媒资工程初始搭建

---
+++ kaizhi-education/kaizhi-media/kaizhi-media-service/src/main/java/com/edu/kaizhi/media/mapper/MediaProcessHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.edu.kaizhi.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.kaizhi.media.model.po.MediaProcessHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessHistoryMapper extends BaseMapper<MediaProcessHistory> {
+
+}
---
+++ kaizhi-education/kaizhi-media/kaizhi-media-service/src/main/java/com/edu/kaizhi/media/mapper/MqMessageMapper.java
@@ -0,0 +1,15 @@
+package com.edu.kaizhi.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.kaizhi.media.model.po.MqMessage;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
